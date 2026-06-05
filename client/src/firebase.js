6a51a3b6711d8c8834a644a9449ff98fa0d60430
YagneshJogi <yagneshjogi1205@gmail.com>
2026-06-05 14:48:05 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBg2arGPdT95nwXfxSFBApkNWORRyD1zms",
-  authDomain: "tddroplet-96223.firebaseapp.com",
-  databaseURL: "https://tddroplet-96223-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tddroplet-96223",
-  storageBucket: "tddroplet-96223.firebasestorage.app",
-  messagingSenderId: "585223952269",
-  appId: "1:585223952269:web:b803f65654e3358cb30fc4"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
